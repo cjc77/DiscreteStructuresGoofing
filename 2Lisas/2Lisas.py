c432fffaa1f52931eb50d1d6e07b5e11553768b8
cjc77 <carsonjamescook@gmail.com>
2017-05-27 17:29:05 -0700

    Unscientific bias is being used.

--- 2Lisas/2Lisas.py
+++ 2Lisas/2Lisas.py
@@ -17,7 +17,9 @@ NUM_MEMS = 172
 NUM_LISA = 8
 NUM_CLASSES = 57
 
-MAX_CLASSES = 3
+MAX_CLASSES = 2
+MAX_CLASS_SIZE = 15
+LIKELY_SIZE = 10
 
 
 def fill_in_members(member_ls):
@@ -39,24 +41,61 @@ def initialize_classes(rosters, members):
     """
     for i in range(NUM_CLASSES):
         # generate random roster size
-        roster_size = random.randrange(1, 16)
+        roster_size = random.randrange(MAX_CLASS_SIZE + 1)
+        # Need to bias against all full classes or many empty classes
+        if roster_size > LIKELY_SIZE:
+            roster_size = biasLarge(roster_size)
+        if roster_size < 1:
+            roster_size = biasSmall(roster_size)
         # create class
         rosters["c" + str(i + 1)] = [-1] * roster_size
     # for k, v in rosters.items():
     #     print(k, v, sep=" -- ")
 
 
+def biasLarge(roll):
+    """
+    Create a 1 in 4 chance of keeping original "roll"
+    """
+    # reRoll or no?
+    reRoll = random.randrange(4)
+    if not reRoll:
+        return roll
+    else:
+        return random.randrange(1, LIKELY_SIZE)
+
+
+def biasSmall(roll):
+    """
+    Create a 1 in 5 chance of keeping original "roll"
+    """
+    #reRoll or no?
+    reRoll = random.randrange(5)
+    if not reRoll:
+        return roll
+    else:
+        return random.randrange(1, LIKELY_SIZE)
+
+
 def fill_class_rosters(rosters, members):
+    """
+    Choose members from the array of members to fill up classes.
+    Bias against members taking > 2 classes each week. Still possible
+    but more unlikely.
+    """
     # go through all classes
     for k in rosters:
         already_chosen = []
         # go through roster of current class
         for i in range(len(rosters[k])):
 
-            # pick a member and check if they've already taken 3 classes
-            # or if they're already signed up for this particular class
+            # pick a member and check if they're already signed up for this class
+            # Also bias against members taking more than 2 classes
             r_member = random.randrange(NUM_MEMS)
-            while r_member in already_chosen or members[r_member][1] == MAX_CLASSES:
+            if members[r_member][1] >= MAX_CLASSES:
+                r_member = random.randrange(NUM_MEMS)
+
+            while r_member in already_chosen:
                 r_member = random.randrange(NUM_MEMS)
 
             # add to the roster
@@ -79,21 +118,27 @@ def check_more_than_one_lisa(rosters):
     return same_name_occurances
 
 
-
 def main():
-    studio_members = [[0] * 2 for i in range(NUM_MEMS)]
-    class_rosters = {}
-    for i in range(100):
-        fill_in_members(studio_members)
-        initialize_classes(class_rosters, studio_members)
-        fill_class_rosters(class_rosters, studio_members)
-        res = check_more_than_one_lisa(class_rosters)
-        print(res)
-        studio_members = [[0] * 2 for i in range(NUM_MEMS)]
-        class_rosters = {}
-    # for k, v in class_rosters.items():
-    #     print(k, v, sep=" -- ")
-
+    years = 100
+    results = []
+    for year in range(years):
+        tests = 52
+        gt1Lisa = 0
+        totalClasses = NUM_CLASSES * tests
+        for i in range(tests):
+            studio_members = [[0] * 2 for i in range(NUM_MEMS)]
+            class_rosters = {}
+            fill_in_members(studio_members)
+            initialize_classes(class_rosters, studio_members)
+            fill_class_rosters(class_rosters, studio_members)
+            res = check_more_than_one_lisa(class_rosters)
+            # for k, v in class_rosters.items():
+            #     print(k, v, len(v), sep=" -- ")
+            if res:
+                gt1Lisa += 1
+        results.append(gt1Lisa / totalClasses)
+    print("Chance of 2 Lisas in a class after 100 test years: ",
+          sum(results) / len(results) * 100, " %", sep=' ')
 
 
 
